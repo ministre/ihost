bb12246b5cc0c212ca762397407e16d7f3d4e328
agorelik <Aleksandr_Gorelik@center.rt.ru>
2024-11-08 00:17:35 +0300

    Initial commit

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -7,5 +7,23 @@ fi
 
 VERSION=$1
 
+# Create control file
+CONTROL_FILE="build/deb-package/DEBIAN/control"
+mkdir -p build/deb-package/DEBIAN  # Create directory if not exist
+
+cat <<EOL > "$CONTROL_FILE"
+Package: ihost 
+Version: $VERSION
+Section: utils
+Priority: optional
+Architecture: amd64
+Depends:
+Maintainer: Aleksandr Gorelik <alx.gorelik@gmail.com>
+Description: Sandbox package. Adding and removing hosts in inventory.yml file for further configuration using Ansible.
+EOL
+
+# Compile
 g++ -o build/deb-package/usr/bin/ihost src/main.cpp
+
+# Create deb-package
 dpkg-deb --build build/deb-package packages/debs/debian/bookworm/ihost_${VERSION}_amd64.deb
